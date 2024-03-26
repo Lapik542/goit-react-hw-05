b493f29b281dc6cafff5c2aa717a031798398fab
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-03-26 18:21:45 +0200

    main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,21 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navigation from '../Navigation/Navigation';
-import HomePage from '../../pages/HomePage/HomePage';
-import MoviesPage from '../../pages/MoviesPage/MoviesPage';
-import MovieDetailsPage from '../MovieDetailsPage/MovieDetailsPage';
-import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = React.lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = React.lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Navigation />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/movies" component={MoviesPage} />
-            <Route exact path="/movies/:movieId" component={MovieDetailsPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </Suspense>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/movies" component={MoviesPage} />
+          <Route path="/movies/:movieId" component={MovieDetailsPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
